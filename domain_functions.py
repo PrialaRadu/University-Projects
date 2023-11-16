17addf4f0c2712be922e27b2205b4c3a41a9d9bb
PrialaRadu <116513225+PrialaRadu@users.noreply.github.com>
2023-11-16 02:20:23 +0200

    Add files via upload

--- domain_functions.py
+++ domain_functions.py
@@ -1,17 +1,18 @@
 # Domain
+from project.variables.get_variables import *
 
 
 # 1 Add Functions
-def add_new_expense(expenses, day, total, type_p):
+def add_new_expense(expenses, day, total, category):
     """
     The function adds a new expense to the list 'expenses'.
     :param expenses: The list that contains all expenses
     :param day: Expense day
     :param total: Total expense.
-    :param type_p: Expense category
+    :param category: Expense category
     :return: No return
     """
-    expenses.append([len(expenses) + 1, day, total, type_p])
+    expenses[get_length(expenses) + 1] = {'day': day, 'total': total, 'category': category}
 
 
 def update_expense(expenses, id_p, new_exp):
@@ -22,18 +23,23 @@ def update_expense(expenses, id_p, new_exp):
     :param new_exp: The new expense values, in format: [id, new_day, new_total, new_type]
     :return: No return
     """
-    expenses[id_p - 1] = [val for exp in expenses if exp[0] == id_p for val in new_exp]
+    expenses[id_p] = new_exp
 
 
 # 2 Remove functions
 def delete_expenses_in_day(expenses, day):
     """
     The function deletes expenses in a specified day.
-    :param expenses: The list containing all expenses
+    :param expenses: The Dictionary containing all expenses
     :param day: Expense day
     :return: No return
     """
-    expenses[:] = [exp for exp in expenses if exp[1] != day]
+    to_be_removed = [key for key, value in expenses.items() if get_day(value) == day]
+
+    for key in to_be_removed:
+        del expenses[key]
+
+    refactor_ids(expenses)
 
 
 def delete_expenses_in_days_range(expenses, start_day, end_day):
@@ -44,31 +50,45 @@ def delete_expenses_in_days_range(expenses, start_day, end_day):
     :param end_day: Ending day of range
     :return: No return
     """
-    expenses[:] = [exp for exp in expenses if exp[1] < start_day or exp[1] > end_day]
+    to_be_removed = [key for key, value in expenses.items() if start_day <= get_day(value) <= end_day]
+
+    for key in to_be_removed:
+        del expenses[key]
+
+    refactor_ids(expenses)
 
 
-def delete_expenses_of_type(expenses, type_p):
+def delete_expenses_of_category(expenses, category):
     """
     The function deletes expenses of a specified category.
     :param expenses: The list containing all expenses
-    :param type_p: Expense category
+    :param category: Expense category
     :return: No return
     """
-    expenses[:] = [exp for exp in expenses if exp[3] != type_p]
+    to_be_removed = [key for key, value in expenses.items() if get_category(value) == category]
+
+    for key in to_be_removed:
+        del expenses[key]
+
+    refactor_ids(expenses)
 
 
 # 3 Search functions
-def print_expenses_greater_than_num(expenses, num):
+def return_expenses_greater_than_num(expenses, num):
     """
     The function returns all expenses that are greater than a specified number.
     :param expenses: The list containing all expenses
     :param num: The number used for comparison
     :return: Returns a new list, with expenses that are greater than 'num'.
     """
-    return [exp for exp in expenses if exp[2] > num]
+    new_dict = {}
+    for key, value in expenses.items():
+        if get_total(value) > num:
+            new_dict.update({key: value})
+    return new_dict
 
 
-def print_exp_gr_than_num_before_day(expenses, day, num):
+def return_exp_gr_than_num_before_day(expenses, day, num):
     """
     The function returns all expenses that are greater than a specified number and are before a specified day.
     :param expenses: The list containing all expenses
@@ -76,74 +96,111 @@ def print_exp_gr_than_num_before_day(expenses, day, num):
     :param num: The number used for comparison
     :return: Returns a new list, containing expenses that are greater than 'num' and are after 'day'.
     """
-    return [exp for exp in expenses if exp[1] < day and exp[2] < num]
+    new_dict = {}
+    for key, value in expenses.items():
+        if get_total(value) > num and get_day(value) < day:
+            new_dict.update({key: value})
+    return new_dict
 
 
-def print_all_expenses_of_type(expenses, type_p):
+def return_all_expenses_of_category(expenses, category):
     """
     The function returns all expenses of a specified category.
     :param expenses: The list containing all expenses
-    :param type_p: Expense category
+    :param category: Expense category
     :return: Returns a new list, containing expenses that are of a specified category.
     """
-    return [exp for exp in expenses if exp[3] == type_p]
+    new_dict = {}
+    for key, value in expenses.items():
+        if get_category(value) == category:
+            new_dict.update({key: value})
+    return new_dict
 
 
 # 4 Reports functions
-def print_sum_of_types(expenses, type_p):
+def return_sum_of_category(expenses, category):
     """
     The function returns the sum of all expenses of a specified category.
     :param expenses: The list containing all expenses
-    :param type_p: Expense category
+    :param category: Expense category
     :return: Returns the sum of all expenses of a specified category.
     """
-    return sum(exp[2] for exp in expenses if exp[3] == type_p)
+    return sum(get_total(value) for key, value in expenses.items() if get_category(value) == category)
 
 
-def find_day_of_sum_max(expenses):
+def return_day_of_sum_max(expenses):
     """
     The function returns the day with the maximum amount of total expenses.
     :param expenses: The list containing all expenses
     :return: Returns the day with the maximum value of total expenses.
     """
-    return max(range(1, 32), key=lambda x: sum(exp[2] for exp in expenses if exp[1] == x))
+    return max(range(1, 32), key=lambda x: sum(get_total(val) for key, val in expenses.items() if get_day(val) == x))
 
 
-def print_exp_equal_to_sum(expenses, num):
+def return_exp_equal_to_num(expenses, num):
     """
     The function returns all expenses that are equal to a specified number.
     :param expenses: The list containing all expenses
     :param num: The number used for comparison
     :return: Returns all expenses that are equal to a specified number.
     """
-    return [exp for exp in expenses if exp[2] == num]
+    new_dict = {}
+    for key, value in expenses.items():
+        if get_total(value) == num:
+            new_dict.update({key: value})
+    return new_dict
 
 
-def print_exp_sorted_by_type(expenses):
+def return_exp_sorted_by_category(expenses):
     """
     The function returns expenses sorted by 'category'.
     :param expenses: The list containing all expenses
     :return: Returns expenses sorted by 'category'.
     """
-    return sorted(expenses, key=lambda x: x[3])
+    sorted_expenses = sorted(expenses.items(), key=lambda x: get_category(x[1]))
+    return dict(sorted_expenses)
 
 
 # 5 Filter functions
-def remove_exp_based_on_type(expenses, type_p):
+def remove_exp_based_on_category(expenses, category):
     """
     The function removes all expenses of a specified category.
     :param expenses: The list containing all expenses
-    :param type_p: Expense category
+    :param category: Expense category
     :return: No return
     """
-    expenses[:] = [exp for exp in expenses if exp[3] != type_p]
+    to_be_removed = [key for key, value in expenses.items() if get_category(value) == category]
+
+    for key in to_be_removed:
+        del expenses[key]
+
+    refactor_ids(expenses)
 
 
-def remove_exp_smaller_than_sum(expenses, num):
+def remove_exp_smaller_than_num(expenses, num):
     """
     The function removes all expenses that are smaller than a specified number.
     :param expenses: The list containing all expenses
     :param num: The number user for comparison
     :return: No return
     """
-    expenses[:] = [exp for exp in expenses if exp[2] > num]
+    to_be_removed = [key for key, value in expenses.items() if get_total(value) < num]
+
+    for key in to_be_removed:
+        del expenses[key]
+
+    refactor_ids(expenses)
+
+
+# Function that updates IDs (if one or more expenses were deleted)
+def refactor_ids(expenses):
+    """
+    The function updates all IDs (if one or more expenses were deleted)
+    :param expenses: The list containing all expenses
+    :return: No return
+    """
+    pos = 1
+    for key in list(expenses):
+        if key != pos:
+            expenses[pos] = expenses.pop(key)
+        pos += 1
--- test_functions.py
+++ test_functions.py
@@ -1,141 +1,214 @@
-# Test functions
+# Test functions that check the integrity of every functionality that prints/modifies the expenses dictionary
+
+from project.functionalities.domain_functions import *
+from project.variables.get_variables import *
 
 
-# test variable 'day'
-def validate_day_variable():
-    """
-    The function continuously prompts the user to input a day value and ensures that the input is a valid integer.
-    If the input is valid, the function returns the valid day value.
-    If the input is invalid, it catches exceptions and prints the corresponding error message.
-    :return: Returns the valid day.
-    """
-    while True:
-        try:
-            day = int(input("Type the day: "))
-            assert 1 <= day <= 31, "Invalid value for day. Please type a value between 1 and 31! "
-            return day
-        except (ValueError, AssertionError) as e:
-            print(e)
+def expenses_example():
+    return {1: {'day': 7,
+                'total': 155.49,
+                'category': 'food'},
+            2: {'day': 11,
+                'total': 54.77,
+                'category': 'phone'},
+            3: {'day': 26,
+                'total': 329.03,
+                'category': 'utilities'}}
 
 
-# test variable 'total'
-def validate_total_variable():
-    """
-    The function continuously prompts the user to input a total value and ensures that the input is a valid float num.
-    If the input is valid, the function returns the valid total value.
-    If the input is invalid, it catches exceptions and prints the corresponding error message.
-    :return: Returns the valid total.
-    """
-    while True:
-        try:
-            total = float(input("Type the total expense: "))
-            assert total > 0, "Invalid value for total expense. Type a value greater than 0! "
-            return total
-        except (ValueError, AssertionError) as e:
-            print(e)
+# Testing 'Add' functions
+def test_add_new_expense():
+    # Test case 1
+    expenses = expenses_example()
+    add_new_expense(expenses, 13, 57, 'food')
+    assert get_length(expenses) == 4, 'Expenses dictionary should have four items after adding'
+    exp = {'day': 13, 'total': 57, 'category': 'food'}
+    assert get_expense(expenses, 4) == exp, 'New expense should be added correctly'
+
+    # Test case 2
+    add_new_expense(expenses, 19, 30.53, 'utilities')
+    assert get_length(expenses) == 5, 'Expenses dictionary should have five items after adding'
+    exp = {'day': 19, 'total': 30.53, 'category': 'utilities'}
+    assert get_expense(expenses, 5) == exp, 'New expense should be added correctly'
 
 
-# test variable 'type_p'
-def validate_type_p_variable():
-    """
-    The function continuously prompts the user to input a category and ensures that the input is a valid string.
-    If the input is valid, the function returns the valid category string.
-    If the input is invalid, it catches exceptions and prints the corresponding error message.
-    :return: Returns the valid category.
-    """
-    while True:
-        try:
-            type_p = str(input("Type the category (food, utilities, clothes, phone, maintenance, others): "))
-            categories = ["food", "utilities", "clothes", "phone", "others", "maintenance"]
-            invalid_mes = "Invalid value for category! (food, utilities, clothes, phone, maintenance, others) "
-            assert type_p in categories, invalid_mes
-            return type_p
-        except (ValueError, AssertionError) as e:
-            print(e)
+def test_update_expense():
+    # Test case 1
+    expenses = expenses_example()
+    new_exp = {'day': 7, 'total': 130, 'category': 'food'}
+    update_expense(expenses, 1, new_exp)
+    assert get_expense(expenses, 1) == new_exp, 'Expense values should be updated correctly'
+
+    # Test case 2
+    expenses = expenses_example()
+    new_exp = {'day': 11, 'total': 54, 'category': 'phone'}
+    update_expense(expenses, 2, new_exp)
+    assert get_expense(expenses, 2) == new_exp, 'Expense values should be updated correctly'
 
 
-# test variable id
-def validate_id_p_variable(exps):
-    """
-    The function continuously prompts the user to input an id and ensures that the input is a valid integer.
-    If the input is valid, the function returns the valid id.
-    If the input is invalid, it catches exceptions and prints the corresponding error message.
-    :return: Returns the valid id.
-    """
-    while True:
-        try:
-            id_p = int(input("Type the id of the expense you want do modify: "))
-            invalid_mes = f"Invalid id. Type a value between 1 and {len(exps)}! "
-            assert 1 <= id_p <= len(exps), invalid_mes
-            return id_p
-        except (ValueError, AssertionError) as e:
-            print(e)
+# Testing 'Remove' functions
+def test_delete_expenses_in_day():
+    # Test case 1
+    expenses = expenses_example()
+    delete_expenses_in_day(expenses, 7)
+    assert get_length(expenses) == 2, 'Expenses dictionary should have two items after removing'
+
+    # Test case 2
+    expenses = expenses_example()
+    delete_expenses_in_day(expenses, 5)
+    assert get_length(expenses) == 3, 'Expenses dictionary should still have three items after removing'
 
 
-# test 'num' variable
-def validate_num_variable():
-    """
-    The function continuously prompts the user to input a value and ensures that the input is a valid number.
-    If the input is valid, the function returns the valid number.
-    If the input is invalid, it catches exceptions and prints the corresponding error message.
-    :return: Returns the valid number.
-    """
-    while True:
-        try:
-            num = float(input("Type the value: "))
-            assert num > 0, "Invalid value. Type a value greater than 0! "
-            return num
-        except (ValueError, AssertionError) as e:
-            print(e)
+def test_delete_expenses_in_days_range():
+    # Test case 1
+    expenses = expenses_example()
+    delete_expenses_in_days_range(expenses, 1, 31)
+    assert get_length(expenses) == 0, 'Expenses dictionary should be empty'
+
+    # Test case 2
+    expenses = expenses_example()
+    delete_expenses_in_days_range(expenses, 1, 10)
+    assert get_length(expenses) == 2, 'Expenses dictionary should have two items after removing'
 
 
-# test 'response' variable
-def validate_response(start, end):
-    """
-    The function continuously prompts the user to input a response and ensures that the input is a valid number.
-    If the input is valid, the function returns the valid response.
-    If the input is invalid, it catches exceptions and prints the corresponding error message.
-    :return: Returns the valid response.
-    """
-    while True:
-        try:
-            resp = int(input())
-            assert start <= resp <= end, f"Invalid response. Type a value between {start} and {end}! "
-            return resp
-        except (ValueError, AssertionError) as e:
-            print(e)
+def test_delete_expenses_of_category():
+    # Test case 1
+    expenses = expenses_example()
+    delete_expenses_of_category(expenses, 'food')
+    assert get_length(expenses) == 2, 'Expenses dictionary should have two items after removing'
+
+    # Test case 2
+    expenses = expenses_example()
+    delete_expenses_of_category(expenses, 'others')
+    assert get_length(expenses) == 3, 'Expenses dictionary should still have three items'
 
 
-# Get user input
-def get_user_expenses(expenses):
-    """
-    Continuously prompts the user to input expense details, validates the inputs and stores the expenses in the list
-    The function uses the following functions for validation:
-        - validate_day_variable
-        - validate_total_variable
-        - validate_type_p_variable
-        - validate_response
-    The expenses are stored in the `user_expenses` list as lists containing:
-        1. Expense ID (automatically assigned based on the length of `user_expenses`).
-        2. Expense day.
-        3. Expense total.
-        4. Expense type.
-    The user is prompted to decide whether to add another expense or modify existing ones.
-    Return: no return
-    """
-    while True:
-        user_day = validate_day_variable()
-        user_total = validate_total_variable()
-        user_type_p = validate_type_p_variable()
+# Testing 'Search' functions
+def test_return_expenses_greater_than_num():
+    # Test case 1
+    expenses = expenses_example()
+    result = return_expenses_greater_than_num(expenses, 100)
+    assert get_length(result) == 2, 'New expenses dictionary should have two items'
 
-        expenses.append([len(expenses) + 1, user_day, user_total, user_type_p])
+    # Test case 2
+    expenses = expenses_example()
+    result = return_expenses_greater_than_num(expenses, 50)
+    assert get_length(result) == 3, 'New expenses dictionary should have three items'
 
-        print(f"\nExpense saved (with ID={len(expenses)}). Do you want to add another one ? \n"
-              "1 - Yes, I want to add another expense! \n"
-              "2 - No, I want to modify/print my expenses! ")
-        resp = validate_response(1, 2)
 
-        if resp == 1:
-            pass
-        elif resp == 2:
-            break
+def test_return_exp_gr_than_num_before_day():
+    # Test case 1
+    expenses = expenses_example()
+    result = return_exp_gr_than_num_before_day(expenses, 20, 100)
+    assert get_length(result) == 1, 'New expenses dictionary should have one item'
+
+    # Test case 2
+    expenses = expenses_example()
+    result = return_exp_gr_than_num_before_day(expenses, 15, 300)
+    assert get_length(result) == 0, 'New expenses dictionary should have no item'
+
+
+def test_return_all_expenses_of_category():
+    # Test case 1
+    expenses = expenses_example()
+    result = return_all_expenses_of_category(expenses, 'food')
+    assert get_length(result) == 1, 'New expenses dictionary should have one item'
+
+    # Test case 2
+    expenses = expenses_example()
+    result = return_all_expenses_of_category(expenses, 'others')
+    assert get_length(result) == 0, 'New expenses dictionary should have no item'
+
+
+# Testing 'Reports' functions
+def test_return_sum_of_category():
+    # Test case 1
+    expenses = expenses_example()
+    result = return_sum_of_category(expenses, 'food')
+    assert result == 155.49, 'Should return the value of "food" expense (155.49)'
+
+    # Test case 2
+    expenses = expenses_example()
+    result = return_sum_of_category(expenses, 'phone')
+    assert result == 54.77, 'Should return the value of "phone" expense (54.77)'
+
+
+def test_return_day_of_sum_max():
+    # Test case 1
+    expenses = expenses_example()
+    result = return_day_of_sum_max(expenses)
+    assert result == 26, 'Should return the day with the maximum total of expenses (day 26)'
+
+
+def test_return_exp_equal_to_num():
+    # Test case 1
+    expenses = expenses_example()
+    result = return_exp_equal_to_num(expenses, 54.77)
+    assert result == {2: {'day': 11, 'total': 54.77, 'category': 'phone'}}, 'Should return the second expense'
+
+    # Test case 2
+    expenses = expenses_example()
+    result = return_exp_equal_to_num(expenses, 155.49)
+    assert result == {1: {'day': 7, 'total': 155.49, 'category': 'food'}}, 'Should return the first expense'
+
+
+def test_return_exp_sorted_by_category():
+    # Test case 1
+    expenses = expenses_example()
+    res = return_exp_sorted_by_category(expenses)
+    assert get_expense(res, 1) == get_expense(expenses, 1), 'The dict should be sorted (first expense)'
+    assert get_expense(res, 2) == get_expense(expenses, 2), 'The dict should be sorted (second expense)'
+    assert get_expense(res, 3) == get_expense(expenses, 3), 'The dict should be sorted (third expense)'
+
+
+# Testing 'Filter' functions
+def test_remove_exp_based_on_category():
+    # Test case 1
+    expenses = expenses_example()
+    remove_exp_based_on_category(expenses, 'food')
+    exp = {'day': 7, 'total': 155.49, 'category': 'food'}
+    assert exp != get_expense(expenses, 1), 'Expense with food category should be removed'
+
+    # Test case 2
+    expenses = expenses_example()
+    remove_exp_based_on_category(expenses, 'others')
+    assert get_length(expenses) == 3, 'Expenses dictionary should remain the same'
+
+
+def test_remove_exp_smaller_than_num():
+    # Test case 1
+    expenses = expenses_example()
+    remove_exp_smaller_than_num(expenses, 200)
+    assert get_length(expenses) == 1, 'Expenses dictionary should have one item'
+
+    # Test case 2
+    expenses = expenses_example()
+    remove_exp_smaller_than_num(expenses, 50)
+    assert get_length(expenses) == 3, 'Expenses dictionary should remain the same'
+
+
+# Test all functions
+def test_all_functionalities():
+    # Test 'add' functions
+    test_add_new_expense()
+    test_update_expense()
+
+    # Test 'remove' functions
+    test_delete_expenses_in_day()
+    test_delete_expenses_in_days_range()
+    test_delete_expenses_of_category()
+
+    # Test 'search' functions
+    test_return_expenses_greater_than_num()
+    test_return_exp_gr_than_num_before_day()
+    test_return_all_expenses_of_category()
+
+    # Test 'reports' functions
+    test_return_sum_of_category()
+    test_return_day_of_sum_max()
+    test_return_exp_equal_to_num()
+    test_return_exp_sorted_by_category()
+
+    # Test 'filter' functions
+    test_remove_exp_based_on_category()
+    test_remove_exp_smaller_than_num()
