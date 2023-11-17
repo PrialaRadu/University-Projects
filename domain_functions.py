18558249a3cb507f1687ac1fe27ab5cd087c59ea
PrialaRadu <116513225+PrialaRadu@users.noreply.github.com>
2023-11-17 08:11:46 +0200

    Update domain_functions.py

--- domain_functions.py
+++ domain_functions.py
@@ -6,7 +6,7 @@ from project.variables.get_variables import *
 def add_new_expense(expenses, day, total, category):
     """
     The function adds a new expense to the list 'expenses'.
-    :param expenses: The list that contains all expenses
+    :param expenses: The dictionary that contains all expenses
     :param day: Expense day
     :param total: Total expense.
     :param category: Expense category
@@ -18,7 +18,7 @@ def add_new_expense(expenses, day, total, category):
 def update_expense(expenses, id_p, new_exp):
     """
     The function updates an expense from the list 'expenses' with new value/values.
-    :param expenses: The list that contains all expenses
+    :param expenses: The dictionary that contains all expenses
     :param id_p: Expense ID
     :param new_exp: The new expense values, in format: [id, new_day, new_total, new_type]
     :return: No return
@@ -30,7 +30,7 @@ def update_expense(expenses, id_p, new_exp):
 def delete_expenses_in_day(expenses, day):
     """
     The function deletes expenses in a specified day.
-    :param expenses: The Dictionary containing all expenses
+    :param expenses: The dictionary containing all expenses
     :param day: Expense day
     :return: No return
     """
@@ -45,7 +45,7 @@ def delete_expenses_in_day(expenses, day):
 def delete_expenses_in_days_range(expenses, start_day, end_day):
     """
     The function deletes expenses in a specified range of days.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :param start_day: Starting day of range
     :param end_day: Ending day of range
     :return: No return
@@ -61,7 +61,7 @@ def delete_expenses_in_days_range(expenses, start_day, end_day):
 def delete_expenses_of_category(expenses, category):
     """
     The function deletes expenses of a specified category.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :param category: Expense category
     :return: No return
     """
@@ -77,7 +77,7 @@ def delete_expenses_of_category(expenses, category):
 def return_expenses_greater_than_num(expenses, num):
     """
     The function returns all expenses that are greater than a specified number.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :param num: The number used for comparison
     :return: Returns a new list, with expenses that are greater than 'num'.
     """
@@ -91,7 +91,7 @@ def return_expenses_greater_than_num(expenses, num):
 def return_exp_gr_than_num_before_day(expenses, day, num):
     """
     The function returns all expenses that are greater than a specified number and are before a specified day.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :param day: Expense day
     :param num: The number used for comparison
     :return: Returns a new list, containing expenses that are greater than 'num' and are after 'day'.
@@ -106,7 +106,7 @@ def return_exp_gr_than_num_before_day(expenses, day, num):
 def return_all_expenses_of_category(expenses, category):
     """
     The function returns all expenses of a specified category.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :param category: Expense category
     :return: Returns a new list, containing expenses that are of a specified category.
     """
@@ -121,7 +121,7 @@ def return_all_expenses_of_category(expenses, category):
 def return_sum_of_category(expenses, category):
     """
     The function returns the sum of all expenses of a specified category.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :param category: Expense category
     :return: Returns the sum of all expenses of a specified category.
     """
@@ -131,7 +131,7 @@ def return_sum_of_category(expenses, category):
 def return_day_of_sum_max(expenses):
     """
     The function returns the day with the maximum amount of total expenses.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :return: Returns the day with the maximum value of total expenses.
     """
     return max(range(1, 32), key=lambda x: sum(get_total(val) for key, val in expenses.items() if get_day(val) == x))
@@ -140,7 +140,7 @@ def return_day_of_sum_max(expenses):
 def return_exp_equal_to_num(expenses, num):
     """
     The function returns all expenses that are equal to a specified number.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :param num: The number used for comparison
     :return: Returns all expenses that are equal to a specified number.
     """
@@ -154,7 +154,7 @@ def return_exp_equal_to_num(expenses, num):
 def return_exp_sorted_by_category(expenses):
     """
     The function returns expenses sorted by 'category'.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :return: Returns expenses sorted by 'category'.
     """
     sorted_expenses = sorted(expenses.items(), key=lambda x: get_category(x[1]))
@@ -165,7 +165,7 @@ def return_exp_sorted_by_category(expenses):
 def remove_exp_based_on_category(expenses, category):
     """
     The function removes all expenses of a specified category.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :param category: Expense category
     :return: No return
     """
@@ -180,7 +180,7 @@ def remove_exp_based_on_category(expenses, category):
 def remove_exp_smaller_than_num(expenses, num):
     """
     The function removes all expenses that are smaller than a specified number.
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :param num: The number user for comparison
     :return: No return
     """
@@ -196,7 +196,7 @@ def remove_exp_smaller_than_num(expenses, num):
 def refactor_ids(expenses):
     """
     The function updates all IDs (if one or more expenses were deleted)
-    :param expenses: The list containing all expenses
+    :param expenses: The dictionary containing all expenses
     :return: No return
     """
     pos = 1
